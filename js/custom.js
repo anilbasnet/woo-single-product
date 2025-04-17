2427e36f65caa6ec5d9125ee039f904e8870592a
Anil Basnet <anilbasnet@Anils-MacBook-Air.local>
2025-04-17 14:09:29 +0545

    Responsive Fixing

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 jQuery(function ($) {
+    // Single Main Slider
   $(".single-main-thumb").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -6,8 +7,8 @@ jQuery(function ($) {
     fade: true,
     dots: false,
     asNavFor: ".pager-thumbnail",
-     prevArrow: '<span data-role="none" class="slick-prev" tabindex="0"></span>',
-	  nextArrow: '<span data-role="none" class="slick-next" tabindex="0"></span>',
+    prevArrow: '<button type="button" class="slick-prev" aria-label="Previous slide"><i class="fa-solid fa-chevron-left" aria-hidden="true"></i></button>',
+    nextArrow: '<button type="button" class="slick-next" aria-label="Next slide"><i class="fa-solid fa-chevron-right" aria-hidden="true"></i></button>',
     rtl: true,
     responsive: [
       {
@@ -20,6 +21,7 @@ jQuery(function ($) {
     ]
   });
 
+  // Single Thumbnail Slider
   $(".pager-thumbnail").slick({
       slidesToShow: 4,
       slidesToScroll: 1,
@@ -29,13 +31,11 @@ jQuery(function ($) {
       focusOnSelect: true,
       arrows: true,
       vertical: true,
-      prevArrow:
-        '<span data-role="none" class="slick-prev" tabindex="0"></span>',
-      nextArrow:
-        '<span data-role="none" class="slick-next" tabindex="0"></span>',
-  
+      prevArrow: '<button type="button" class="slick-prev" aria-label="Previous slide"><i class="fa-solid fa-chevron-up"></i></button>',
+      nextArrow: '<button type="button" class="slick-next" aria-label="Next slide"><i class="fa-solid fa-chevron-down"></i></button>',
   });
 
+   // Related Post Carousel
   $(".section-related-product-carousel").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -45,33 +45,32 @@ jQuery(function ($) {
     arrows: true,
     infinite: true,
     autoplay: true,
-  autoplaySpeed: 2000,
-    prevArrow:
-      '<span data-role="none" class="slick-next" tabindex="0"></span>',
-    nextArrow:
-      '<span data-role="none" class="slick-prev" tabindex="0"></span>',
-      rtl: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 650,
-          settings: {
-            arrows: false,
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-next" aria-label="Previous slide"><i class="fa-solid fa-chevron-right" aria-hidden="true"></i></button>',
+    nextArrow: '<button type="button" class="slick-prev" aria-label="Next slide"><i class="fa-solid fa-chevron-left" aria-hidden="true"></i></button>',
+    rtl: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
         }
-      ]
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      
+      }
+    ]
   });
 
+ //Faq's
   $('.faq-list').find('.faq-content').hide();
   $(document).on('click', '.faq-title', function() {
     $(this).parent().siblings().removeClass('acc_active');
@@ -81,7 +80,7 @@ jQuery(function ($) {
     $(this).next('.faq-content').slideToggle(200);
   });
 
-
+ // Quantity
   $('.minus').click(function () {
     var $input = $(this).parent().find('input');
     var count = parseInt($input.val()) - 1;
@@ -90,7 +89,6 @@ jQuery(function ($) {
     $input.change();
     return false;
   });
-  
   $('.plus').click(function () {
     var $input = $(this).parent().find('input');
     $input.val(parseInt($input.val()) + 1);
